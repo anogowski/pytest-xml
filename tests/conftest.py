0dabdfccc7622641de84305ae905a970d01cfc57
anogowski <anogowski@live.com>
2024-10-10 21:56:38 -0600

    Initial Setup

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+#############################################
+#	Dual License: BSD-3-Clause AND MPL-2.0	#
+#	Copyright (c) 2024, Adam Nogowski		#
+#############################################
+
+pytest_plugins = 'pytester'
